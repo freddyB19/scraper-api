b51d59ae5b054fccd5ae8fd6988171941c9c74bb
freddyB19 <freddyybolivar2013@gmail.com>
2026-03-09 19:05:47 -0400

    config(project): agregando configuración para los 'cors'

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,12 +1,20 @@
+from typing import Annotated
+
+from pydantic import field_validator
 
 from dotenv import load_dotenv
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, NoDecode
 
 load_dotenv()
 
 class Config(BaseSettings):
 	url: str
-
+	cors_origins: Annotated[list[str], NoDecode]
+	
+	@field_validator('cors_origins', mode='before')
+	@classmethod
+	def decode_cors_origins(cls, domains: str) -> list[str]:
+		return [domain for domain in domains.split(',') if domain]
 
 config = Config()
 
--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi_pagination import add_pagination
 
+from core.settings import config
 from routes.api.router import router
 from routes.graphql.schemas import graphql_app
 
 app = FastAPI()
+
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins = config.cors_origins,
+	allow_methods = ['GET', 'POST'],
+	allow_headers = ['Content-Type'],
+)
 
 app.include_router(router)
 app.include_router(graphql_app, prefix = "/graphql")
